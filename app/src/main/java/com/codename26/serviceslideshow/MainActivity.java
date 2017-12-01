22c751fdb57d2af78f9efcc0cc0a9e4b46a62760
Codename26 <slava.burchenko@gmail.com>
2017-12-01 20:20:36 +0200

    Added Service and binded to it. Added nextImage and PrevImage methods to service.

--- app/src/main/java/com/codename26/serviceslideshow/MainActivity.java
+++ app/src/main/java/com/codename26/serviceslideshow/MainActivity.java
@@ -1,14 +1,103 @@
 package com.codename26.serviceslideshow;
 
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.net.Uri;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Slide;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String IMAGE_URI = "IMAGE_URI";
+    private Button btnPrev;
+    private Button btnNext;
+    private Button btnSlideShow;
+    private ImageView mImageView;
+    static final int TASK_NEXT = 1;
+    static int TASK_PREV = 2;
+    public static final String PARAM_PINTENT = "PARAM_PINTENT";
+    SlideService mService;
+    boolean mBound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initButtons();
         //test
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(MainActivity.this, SlideService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
+
+    private void initButtons() {
+        btnPrev = findViewById(R.id.buttonPrev);
+        btnNext = findViewById(R.id.buttonNext);
+        btnSlideShow = findViewById(R.id.buttonSlideShow);
+        mImageView = findViewById(R.id.imageView);
+
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mBound){
+                    mImageView.setImageURI(mService.getPrevImage());
+                }
+
+            }
+        });
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mBound){
+                    mImageView.setImageURI(mService.getNextImage());
+                }
+
+            }
+        });
+        btnSlideShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            SlideService.LocalBinder binder = (SlideService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+        }
+    };
 }
---
+++ app/src/main/java/com/codename26/serviceslideshow/SlideService.java
@@ -0,0 +1,55 @@
+package com.codename26.serviceslideshow;
+
+import android.app.Service;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.io.File;
+import java.net.URI;
+import java.util.Random;
+
+public class SlideService extends Service {
+    private final IBinder mBinder = new LocalBinder();
+    private String[] imageUris = {"/sdcard/Download/1.png", "/sdcard/Download/2.png","/sdcard/Download/3.png",
+            "/sdcard/Download/4.png", "/sdcard/Download/5.png"};
+    private int current = 0;
+    private int prev = 0;
+    public SlideService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("Service Status", "Service Stopped");
+    }
+
+    public class LocalBinder extends Binder{
+        SlideService getService(){
+            return SlideService.this;
+        }
+    }
+
+    public Uri getNextImage(){
+        Log.d("current index", String.valueOf(current));
+        int nextImageIndex = current;
+        while (nextImageIndex == current){
+            nextImageIndex = new Random().nextInt(5);
+        }
+        prev = current;
+        current = nextImageIndex;
+        return Uri.parse(new File(imageUris[nextImageIndex]).toString());
+    }
+    public Uri getPrevImage(){
+        Log.d("prev index", String.valueOf(prev));
+        return Uri.parse(new File(imageUris[prev]).toString());
+    }
+}
